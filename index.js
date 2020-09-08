abb02f195501b962187c8ea77c10f6f6ecc58a86
Jake Grella <jgrella13@gmail.com>
2020-09-08 18:58:57 -0500

    fixed error in problem 1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 let votingAge = 18;
-if(age > 18){
+if(age >= 18){
     console.log(true);
 }
 
